9e9e36e22e7773d7c7261d4ed32bf74de8dbb9de
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    sm6250-common: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -176,7 +176,7 @@ VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
