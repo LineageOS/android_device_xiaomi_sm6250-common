738518e77d9622c9a338faf785072c710bd86ab9
Arian <arian.kulmer@web.de>
2022-03-07 14:00:40 +0100

    sm6350-common: Build GPS HAL from source and import proprietary configs
    Configs from curtana V14.0.4.0.SJWMIXM
    slim_daemon is disabled in izat.conf, hence drop it.
    
    Change-Id: Ia1ed33f6250b898d149970389befe8033cc8eaf3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,10 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
