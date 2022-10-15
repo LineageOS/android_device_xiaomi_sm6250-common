c1845baf412b6d14ce88f80530d621d42f76ceff
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    sm6250-common: Prevent adding same dependency twice in blob_fixup
    Change-Id: Idedeb51e027a09f64a29ac67a3a2f2a0594213f6

--- extract-files.sh
+++ extract-files.sh
@@ -64,7 +64,7 @@ function blob_fixup() {
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
-            "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         system_ext/etc/init/wfdservice.rc)
             sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
