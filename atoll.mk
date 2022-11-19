85e0d1a05cdafbc3647f638b87c9eabaca6b037c
Alexander Winkowski <dereference23@outlook.com>
2022-11-19 17:49:27 +0000

    Revert "Revert "sm6250-common: Drop all component overrides""
    This reverts commit 546dbf87aa86274512298d9d7c32bb014a67d629.
    
    Now it's fine on Android 13.
    
    Change-Id: I19289c172daa80d0c5a074cd40f482745dfe755c

--- atoll.mk
+++ atoll.mk
@@ -81,10 +81,6 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libstdc++.vendor
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
