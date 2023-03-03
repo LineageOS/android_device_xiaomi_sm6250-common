a2bf24d262563d06d37d9d36e9b37ff871883df8
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sm6250-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- atoll.mk
+++ atoll.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
