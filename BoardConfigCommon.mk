f54100b3dd890a20d90908103a0efc442c987ad2
Alexander Winkowski <dereference23@outlook.com>
2023-08-09 10:16:03 +0000

    sm6250-common: Update blobs from V14.0.4.0.SJWMIXM
    Change-Id: I292e80f455cc4063a7604d42864cd858dbf25726

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -168,7 +168,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-06-05
+VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
