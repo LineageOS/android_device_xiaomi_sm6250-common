ea2495b62e9c9fd89bac894ea54e9501e439abdd
Giovanni Ricca <giovanniricca@proton.me>
2023-08-24 02:46:30 +0200

    sm6250-common: Call lineage framework compatibility matrix xml from vendor/lineage
    Change-Id: I86552803fa67fbdacb1383755bd60b5ef0df8ec8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,9 +68,9 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    $(COMMON_PATH)/lineage_device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
