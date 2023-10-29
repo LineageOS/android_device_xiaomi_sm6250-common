7ff082836e09576e26e920576cfbec5ec63a02ba
Giovanni Ricca <giovanniricca@proton.me>
2023-10-29 17:24:19 +0100

    sm6250-common: Don't let patchelf corrupt the lib
    Change-Id: I268616bdde695c14121f4a337d1fb1c2897ceacd

--- atoll.mk
+++ atoll.mk
@@ -430,7 +430,7 @@ PRODUCT_COPY_FILES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/v32hidlbase.so
 
 # WiFi
 PRODUCT_PACKAGES += \
