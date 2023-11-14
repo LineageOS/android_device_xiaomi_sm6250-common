77fbcf09484169692689e02c8a828674dc4916f2
Giovanni Ricca <giovanniricca@proton.me>
2023-11-14 20:56:40 +0000

    Revert "sm6250-common: Define PID 1.0 thermal HAL feature"
    This reverts commit f6d269ea77c7b56a5ca4f72fb7299f5ce73e0331.
    
    Change-Id: Iff451d04f89367bd4b7200bdb1116b8e0267d70a

--- atoll.mk
+++ atoll.mk
@@ -391,8 +391,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal HAL
-$(call soong_config_set,thermal_hal_feature,pid,apply_1_0)
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
     thermal_symlinks
