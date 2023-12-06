8bffd3cd8a57450f1c232c34c0a6751a5326d1c0
Giovanni Ricca <giovanniricca@proton.me>
2023-12-06 09:50:13 +0100

    sm6250-common: Build goodix lib from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e

--- atoll.mk
+++ atoll.mk
@@ -127,6 +127,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
