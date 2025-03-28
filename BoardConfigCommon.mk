a9dd65448f0407d19988a962a00bf104a6ef95ff
Giovanni Ricca <giovanniricca@proton.me>
2025-03-28 21:54:20 +0100

    sm6250-common: Temporarily downgrade kernel clang version
    19.0.1 doesn't boot for us. Let's keep using the old one for now.
    
    Change-Id: I6693b36674539c20870be49d564b652a93478de1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
+TARGET_KERNEL_CLANG_VERSION := r530567
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
