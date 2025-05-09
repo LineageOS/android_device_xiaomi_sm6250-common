76433d40777d60173514dc94b83b4ed3b0446758
Giovanni Ricca <giovanniricca@proton.me>
2025-05-09 22:06:58 +0200

    sm6250-common: Unset vendor init lib
    Change-Id: Id88f547f9708dd71a4ca5476f12882bafe4e2ba8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,9 +57,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_atoll
-
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
