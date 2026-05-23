71558d66fc760f17b58eea141ea1d731dbb473b5
Onelots <onelots@lineageos.org>
2026-05-23 18:18:29 +0200

    sm6250-common: Include miatoll defconfig
    Change-Id: I96d61eca14598b1722bb6eb305bd72a80e938331

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,6 +69,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
+TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
