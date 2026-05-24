13941951532b0fa611b9911431440752348d7f8d
Onelots <onelots@lineageos.org>
2026-05-24 02:29:33 +0200

    sm6250-common: Migrate LineageSettingsProvider overlay to RRO
    Change-Id: Ifa1851582664c85027783dd0f1b6ad9fdd7a7ec2
    Signed-off-by: Onelots <onelots@lineageos.org>

--- atoll.mk
+++ atoll.mk
@@ -158,10 +158,10 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    LineageSettingsProvider \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
